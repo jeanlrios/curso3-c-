d2fdd2f8ab88796822d1acaff34f7799975fe57d
Jean Lucas Rios <161527650+jeanlrios@users.noreply.github.com>
2024-04-19 11:53:52 -0300

    Add files via upload

--- ScreenSound V2/ScreenSound/Modelos/Album.cs
+++ ScreenSound V2/ScreenSound/Modelos/Album.cs
@@ -1,7 +1,8 @@
 ﻿namespace ScreenSound.Modelos;
-internal class Album
+internal class Album : IAvaliavel
 {
     private List<Musica> musicas = new List<Musica>();
+    private List<Avaliacao> notas = new List<Avaliacao>();
     public Album(string nome)
     {
         Nome = nome;
@@ -9,10 +10,26 @@ internal class Album
     public string Nome { get; }
     public int DuracaoTotal => musicas.Sum(m => m.Duracao);
     public List<Musica> Musicas => musicas;
+
+    public double Media
+    {
+        get
+        {
+            if (notas.Count == 0) return 0;
+            else return notas.Average(a => a.Nota);
+        }
+    }
+
     public void AdicionarMusica(Musica musica)
     {
         musicas.Add(musica);
     }
+
+    public void AdicionarNota(Avaliacao nota)
+    {
+        notas.Add(nota);
+    }
+
     public void ExibirMusicasDoAlbum()
     {
         Console.WriteLine($"Lista de músicas do álbum {Nome}:\n");
